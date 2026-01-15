79b6db918681eb8e0a49a8fb345416f65875ed00
Daniel Afonso <danielafonso@MacBook-Air-de-Daniel.local>
2026-01-15 14:29:18 +0100

    starting cpp07

---
+++ ex00/includes/whatever.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danielafonso <danielafonso@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/15 11:49:53 by danielafons       #+#    #+#             */
+/*   Updated: 2026/01/15 14:26:51 by danielafons      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+#include <iostream>
+
+template <typename T> T myMax();
+
+#endif
