01ec0ecd8a86c06bd62eda3463661aa4efaeaf23
Daniel Afonso <daniel-congo2@hotmail.com>
2026-02-06 19:56:07 +0100

    building ex01

--- ex01/includes/iter.hpp
+++ ex01/includes/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: danielafonso <danielafonso@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 11:49:53 by danielafons       #+#    #+#             */
-/*   Updated: 2026/02/05 14:28:28 by danielafons      ###   ########.fr       */
+/*   Updated: 2026/02/06 12:36:21 by danielafons      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,9 @@
 #include <iostream>
 
 template <typename T>
-void iter(T *a, const T b, void (*f)(T&)){
-    for (size_t i = 0; i < b; i++)
-    {
+void iter(T *a, const size_t len, void (*f)(const T&)){
+    for (size_t i = 0; i < len; i++)
         f(a[i]);
-    }
 };
 
 
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,21 +6,49 @@
 /*   By: danielafonso <danielafonso@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 11:48:27 by danielafons       #+#    #+#             */
-/*   Updated: 2026/02/05 14:15:23 by danielafons      ###   ########.fr       */
+/*   Updated: 2026/02/06 15:55:12 by danielafons      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/iter.hpp"
 
-void print_value(const T& value)
+template <typename T>
+
+void print_value(const std::string& str)
 {
-    std::cout << value << std:endl;
+    std::cout << str << std::endl;
+}
+
+void ft_toupper(std::string& str)
+{
+   for (size_t i = 0; str[i] != '\0'; i++)
+        std::toupper(str[i]);
+}
+
+void test_string_array()
+{
+    std::cout << "\033[32m" << "Test string array\n"<< "\033[0m";
+    std::string str[] = {"Hello,", "my friend"};
+    std::string str1[] = {"I am,", "Naruto Uzumaki"};
+    const size_t len = sizeof(str) / sizeof(str[0]);
+
+    ::iter(str, len, print_value);
+    std::cout << "----------------------------\n";
+    ::iter(str1, len, ft_toupper);
+}
+
+void test_int_array()
+{
+    std::cout << "\033[32m" << "Test int array\n"<< "\033[0m";
+    int numbers[] = {12, 32, 45, 9};
+    const size_t len = sizeof(numbers) / sizeof(numbers[0]);
+
+    ::iter(numbers, len, print_value);
 }
 
 int main(void)
 {
-    std::string str = "Hello";
-    
-    ::iter(str, str.size(), print_value);
+    test_string_array();
+    test_int_array();
     return (0);
 }
